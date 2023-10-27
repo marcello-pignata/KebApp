964eadd9e70bb96b0d3407a3db172a9d16e275af
Marcello Pignata <72017683+MarcelloPignata@users.noreply.github.com>
2023-10-27 10:50:11 +0200

    Aggiunta bozza schermata login

--- app/src/main/java/com/example/kebapp/LoginActivity.java
+++ app/src/main/java/com/example/kebapp/LoginActivity.java
@@ -3,12 +3,38 @@ package com.example.kebapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputLayout;
+
+public class LoginActivity extends AppCompatActivity
+{
+    private final String TAG = "LoginActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextInputLayout textInputLayoutUsername = findViewById(R.id.textInputLayoutUsername);
+        TextInputLayout textInputLayoutPassword = findViewById(R.id.textInputLayoutPassword);
+        textInputLayoutUsername.setErrorIconDrawable(0);
+        textInputLayoutPassword.setErrorIconDrawable(0);
+
+
+        // ONCLICK PULSANTE LOGIN
+        findViewById(R.id.buttonLogin).setOnClickListener(item ->
+        {
+            String username = textInputLayoutUsername.getEditText().getText().toString();
+            String password = textInputLayoutPassword.getEditText().getText().toString();
+
+            Log.d(TAG, "Login: {" + username + ", " + password + "}");
+
+            /*
+            textInputLayoutUsername.setError("username non valido");
+            textInputLayoutPassword.setError("password errata");
+            */
+        });
     }
 }
