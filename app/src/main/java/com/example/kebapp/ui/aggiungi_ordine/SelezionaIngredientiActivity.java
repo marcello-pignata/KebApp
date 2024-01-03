8c9677c0d84820c7fa4d9e3f255e7c65eba1d34e
Developing303 <d.salaj@campus.unimib.it>
2024-01-03 14:17:41 +0100

    Aggiunta # telefono, fattorino, note a card_ordine

--- app/src/main/java/com/example/kebapp/ui/aggiungi_ordine/SelezionaIngredientiActivity.java
+++ app/src/main/java/com/example/kebapp/ui/aggiungi_ordine/SelezionaIngredientiActivity.java
@@ -5,20 +5,20 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.kebapp.Ingrediente;
+import com.example.kebapp.Prodotto;
 import com.example.kebapp.R;
 
 import java.util.ArrayList;
 
 public class SelezionaIngredientiActivity extends AppCompatActivity
 {
-    // tag usato per debugging
-    private final String TAG = "SelezionaIngredientiActivity";
+    ArrayList<Ingrediente> listaAggiunte;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,51 +26,55 @@ public class SelezionaIngredientiActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleziona_ingredienti);
 
-        // ottengo i dati passati da RecyclerViewProdottiAdapter
         ArrayList<Ingrediente> listaIngredienti = (ArrayList<Ingrediente>)getIntent().getSerializableExtra("listaIngredienti");
         ArrayList<Ingrediente> listaAggiunte = (ArrayList<Ingrediente>)getIntent().getSerializableExtra("listaAggiunte");
         int idProdotto = getIntent().getIntExtra("idProdotto", -1);
 
-        // per ogni ingrediente disponibile creo una nuova card da aggiungere all'activity
+        LinearLayout linearLayoutIngredienti = findViewById(R.id.linearLayoutIngredienti);
         CardView newCard;
         TextView newTextView;
+
+        findViewById(R.id.buttonAggiungiAggiunte).setOnClickListener(item ->
+        {
+            Intent resultIntent = new Intent();
+            resultIntent.putExtra("listaAggiunte", listaAggiunte);
+            resultIntent.putExtra("idProdotto", idProdotto);
+            setResult(RESULT_OK, resultIntent);
+            finish();
+        });
+
+
         for (int i=0; i < listaIngredienti.size(); i++)
         {
-            // inizializzo la nuova card e le assegno il layout definito in card_ingrediente.xml
             newCard = new CardView(getApplicationContext());
             getLayoutInflater().inflate(R.layout.card_ingrediente, newCard);
 
-            // imposto il testo della text view per il nome dell'ingrediente
-            newTextView = newCard.findViewById(R.id.textViewNomeProdotto);
+            newTextView = newCard.findViewById(R.id.textViewNomeIngrediente);
             newTextView.setText(listaIngredienti.get(i).nome);
 
-            // imposto il testo della text view per il prezzo dell'ingrediente
-            newTextView = newCard.findViewById(R.id.textViewPrezzoProdotto);
+            newTextView = newCard.findViewById(R.id.textViewPrezzoIngrediente);
             newTextView.setText(listaIngredienti.get(i).prezzo + "€");
 
-            // inizializzo la checkbox della card
+            linearLayoutIngredienti.addView(newCard);
+
+            int finalI = i;
+
             CheckBox newCheckBox = newCard.findViewById(R.id.checkBoxSelezionaIngrediente);
 
-            // se tra la lista delle aggiunte già esistenti è presente l'ingrediente di questa card
-            // allora imposto la sua checkbox come già spuntata
             for (int j = 0; j < listaAggiunte.size(); j++)
             {
-                if(listaAggiunte.get(j).nome.equals(listaIngredienti.get(i).nome))
+                if(listaAggiunte.get(j).nome.equals(listaIngredienti.get(finalI).nome))
                 {
                     newCheckBox.setChecked(true);
                 }
             }
 
-            // setOnClickListener della checkbox della card
-            int finalI = i;
             newCheckBox.setOnClickListener(item ->
             {
-                // se la checkbox viene spuntata allora aggiungo il corrispondente ingrediente a listaAggiunte
                 if (newCheckBox.isChecked())
                 {
                     listaAggiunte.add(listaIngredienti.get(finalI));
                 }
-                // se la checkbox viene deselezionata allora rimuovo il corrispondente ingrediente da listaAggiunte
                 else
                 {
                     for (int j = 0; j < listaAggiunte.size(); j++)
@@ -82,22 +86,7 @@ public class SelezionaIngredientiActivity extends AppCompatActivity
                     }
                 }
             });
-
-            // aggiungo la nuova card al layout
-            ((LinearLayout)findViewById(R.id.linearLayoutIngredienti)).addView(newCard);
         }
 
-        // setOnClickListener del bottone per aggiungere gli ingredienti
-        findViewById(R.id.buttonAggiungiAggiunte).setOnClickListener(item ->
-        {
-            // termino l'esecuzione dell'activity, ritornando come result:
-            //      - l'elenco delle aggiunte modificato
-            //      - l'ID del prodotto che vogliamo modificare
-            Intent resultIntent = new Intent();
-            resultIntent.putExtra("listaAggiunte", listaAggiunte);
-            resultIntent.putExtra("idProdotto", idProdotto);
-            setResult(RESULT_OK, resultIntent);
-            finish();
-        });
     }
 }
